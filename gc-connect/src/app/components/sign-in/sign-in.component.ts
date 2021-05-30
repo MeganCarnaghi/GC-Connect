3f2e81eb70954fcf38e9ed5f37999e5363a698a0
Megan <carnagh5@gmail.com>
2021-05-30 18:08:36 -0400

    Updated firebase authentication and deleted login component

--- gc-connect/src/app/components/sign-in/sign-in.component.ts
+++ gc-connect/src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgAuthService } from '../../services/ng-auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,7 +9,15 @@ import { NgAuthService } from '../../services/ng-auth.service';
   styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
-  constructor(public ngAuthService: NgAuthService) {}
+  constructor(
+    public ngAuthService: NgAuthService,
+    public afAuth: AngularFireAuth
+  ) {}
 
   ngOnInit() {}
+
+  signInWithGoogle() {
+    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+    this.afAuth.signInWithPopup(googleAuthProvider);
+  }
 }
