caf669f986967ee309081b7ce317df87b23d599e
Megan <carnagh5@gmail.com>
2021-05-30 11:49:03 -0400

    added aditional components for sign in

--- gc-connect/src/app/app.module.ts
+++ gc-connect/src/app/app.module.ts
@@ -10,9 +10,21 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { environment } from '../environments/environment';
 import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { NgAuthService } from '../app/services/ng-auth.service';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    SignInComponent,
+    VerifyEmailComponent,
+    ForgotPasswordComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,7 +35,7 @@ import { LoginComponent } from './components/login/login.component';
     AngularFireStorageModule,
     AngularFireAuthModule,
   ],
-  providers: [],
+  providers: [NgAuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
