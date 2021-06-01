2fe84e13ceb615ef5160f96b84cd5241fb6fdfac
kkholland <katherinebholland@gmail.com>
2021-06-01 13:15:32 -0400

    added postgres routes, connected in gc-connect service.ts to local host, and added new gc-connect service into profile component

--- gc-connect/src/app/services/gc-connect.service.ts
+++ gc-connect/src/app/services/gc-connect.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GcConnectService {
   constructor(private client: HttpClient) {}
+
+  getAllUsers(){
+    return this.client.get('http://localhost:3000/users');
+  };
+
+  getAllGroups(){
+    return this.client.get('http://localhost:3000/users');
+  };
+
+  getUserByUid(user: User){
+    let uid = user.uid;
+
+    return this.client.get(`http://localhost:3000/user/${uid}`);
+  };
+
+  getGroupById(group: any){
+    let id = group.id;
+
+    return this.client.get(`http://localhost:3000/group/${id}`);
+  };
+
+  getGroupPostsById(group: any){
+    let id = group.id;
+
+    return this.client.get(`http://localhost:3000/group-posts/${id}`);
+  };
+
 }
---
+++ routes.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const pgp = require('pg-promise')();
+const router = express.Router();
+
+router.use(express.json());
+
+// *** Database here
+const db = pgp({
+    database: 'gc-connect' 
+});
+
+// get all users for phonebook list
+router.get('/users', async (req, res) => {
+    res.json(await db.many('SELECT * from users'));
+});
+
+// get all groups for group list
+router.get('/groups', async (req, res) => {
+    res.json(await db.many('SELECT * from groups'));
+});
+
+// get user by id for profile/popup
+router.get('/user/:uid', async (req, res) => {
+    const result = await db.one('SELECT * from users WHERE firebase_uid = $(uid)', {
+        uid: req.params.uid
+    });
+
+    if (!result) {
+        return res.status(404).send('The user could not be found');
+    }
+
+    res.json(result);
+});
+
+// get group by id for details
+router.get('/group/:id', async (req, res) => {
+    const result = await db.one('SELECT * from groups WHERE id = $(id)', {
+        id: req.params.id
+    });
+
+    if (!result) {
+        return res.status(404).send('The group could not be found');
+    }
+
+    res.json(result);
+});
+
+// get group-posts by group
+router.get('/group-posts/:id', async (req, res) => {
+    const result = await db.many('SELECT * from group_posts WHERE group_id = $(id)', {
+        id: req.params.id
+    });
+
+    if (!result) {
+        return res.status(404).send('The group could not be found');
+    }
+
+    res.json(result);
+});
+
+
+
+
+module.exports = router;
