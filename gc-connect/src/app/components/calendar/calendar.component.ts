6677ab34da6a89b281d9434942e1e71d7202875c
pbla2394@gmail.com <pbla2394@gmail.com>
2021-06-02 08:49:41 -0400

    integrated fullcalendar to the calendar component

--- gc-connect/src/app/components/calendar/calendar.component.ts
+++ gc-connect/src/app/components/calendar/calendar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.css']
+  styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
