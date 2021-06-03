e11ce53c88d5e331a511d566335fa32507bc6970
Megan <carnagh5@gmail.com>
2021-06-02 22:01:06 -0400

    updated sign in and sign up components, style email verification and password reset components

--- gc-connect/src/app/components/sign-up/sign-up.component.ts
+++ gc-connect/src/app/components/sign-up/sign-up.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NgAuthService } from '../../services/ng-auth.service';
-import { AngularFireAuth } from '@angular/fire/auth';
-import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-sign-up',
@@ -11,24 +9,7 @@ import firebase from 'firebase/app';
 export class SignUpComponent implements OnInit {
   userState: any;
 
-  constructor(
-    public ngAuthService: NgAuthService,
-    public afAuth: AngularFireAuth
-  ) {
-    this.afAuth.authState.subscribe((user) => {
-      if (user) {
-        this.userState = user;
-        localStorage.setItem('user', JSON.stringify(this.userState));
-      } else {
-        localStorage.removeItem('user');
-      }
-    });
-  }
+  constructor(public ngAuthService: NgAuthService) {}
 
   ngOnInit(): void {}
-
-  signUpWithGoogle() {
-    const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-    this.afAuth.signInWithPopup(googleAuthProvider);
-  }
 }
