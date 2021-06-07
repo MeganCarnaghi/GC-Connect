71b1ee12ecd8f36335247669487732170fe55a6d
pbla2394@gmail.com <pbla2394@gmail.com>
2021-06-07 19:43:25 -0400

    added placeholder inputs forprofile page

--- gc-connect/src/app/components/profile/profile.component.ts
+++ gc-connect/src/app/components/profile/profile.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { GcConnectService } from 'src/app/services/gc-connect.service';
-
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  faUser = faUser;
 
-  constructor(private SQLservice: GcConnectService ) { }
+  constructor(private SQLservice: GcConnectService) {}
 
-  ngOnInit(): void {
-    
-  }
-
+  ngOnInit(): void {}
 }
