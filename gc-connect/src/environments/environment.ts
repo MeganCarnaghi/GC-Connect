90d544b086861d5c987f8c0dbb4690426f590002
Megan <carnagh5@gmail.com>
2021-06-09 18:08:05 -0400

    cleaned up environment files

--- gc-connect/src/environments/environment.ts
+++ gc-connect/src/environments/environment.ts
@@ -3,19 +3,3 @@ import { databases } from '../environments/api.environment';
 export const environment = {
   ...databases,
 };
-
-// firebase: {
-//   apiKey: 'AIzaSyDoSKR5FTannTDkdBuQOI5K4tbU-TK2KY4',
-//   authDomain: 'gc-connect-315022.firebaseapp.com',
-//   projectId: 'gc-connect-315022',
-//   storageBucket: 'gc-connect-315022.appspot.com',
-//   messagingSenderId: '4729421608',
-// },
-
-// googleCalendar: {
-//   apiKey: 'AIzaSyCKRYZ_1cqytLIn4R8YlqrKo6Ha9hdUIv4',
-// },
-
-// filestack: {
-//   apiKey: 'AnjkxIXpGRNeLQsUSfgk7z',
-// },
