7ba04ef8b55cdd139858ef3ec7fbe667a12b6c11
pbla2394@gmail.com <pbla2394@gmail.com>
2021-06-18 13:37:49 -0400

    added form styling to create group component

--- gc-connect/src/app/components/create-group/create-group.component.ts
+++ gc-connect/src/app/components/create-group/create-group.component.ts
@@ -65,7 +65,6 @@ export class CreateGroupComponent implements OnInit {
         groupBio,
         groupPhoto
       ).subscribe((group) => (this.groups = group));
-      alert('Group has been created!');
     }
   }
 
