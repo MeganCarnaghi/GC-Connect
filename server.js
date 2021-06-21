858c98684ddc0a8cf5133d1ba9b33a8dbfecb5d8
Megan <carnagh5@gmail.com>
2021-06-21 13:46:56 -0400

    updating port in server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Required Dependencies
 const express = require("express");
 const cors = require("cors");
-const router = require('./routes');
+const router = require("./routes");
 
 // Create instance of Express server
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use("/", router);
 
 // Define the port
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Run the server
 app.listen(port, () => console.log(`Listening on port: ${port}.`));
